160606b3913193201d1adf604ea784eeb42bbba2
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-09-14 11:11:06 -0500

    move rawg stuff to a folder + add types

---
+++ src/app/rawg/rawg-game.interface.ts
@@ -0,0 +1,9 @@
+export interface RawgGame {
+  id: number;
+  slug?: string;
+  name: string;
+  name_original?: string;
+  description?: string;
+  background_image?: string;
+  background_image_additional?: string;
+}
--- src/app/rawg/rawg.service.ts
+++ src/app/rawg/rawg.service.ts
@@ -1,21 +1,26 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { RawgGame } from "./rawg-game.interface";
+import { RawgListResponse } from "./rawg-list-response.interface";
 
 const BASE_URL = "https://api.rawg.io/api";
-const key = "API_KEY";
+const key = "c95ca9de0a4048918ca678918e43415b";
 
 @Injectable({
   providedIn: "root",
 })
 export class RawgService {
   constructor(private http: HttpClient) {}
+
   getGameList({ page = 1, search = "" }: { page?: number; search?: string }) {
-    return this.http.get(`${BASE_URL}/games`, {
+    return this.http.get<RawgListResponse<RawgGame>>(`${BASE_URL}/games`, {
       params: { key, page, search },
     });
   }
 
   getGame(id: string) {
-    return this.http.get(`${BASE_URL}/games/${id}`, { params: { key } });
+    return this.http.get<RawgGame>(`${BASE_URL}/games/${id}`, {
+      params: { key },
+    });
   }
 }
